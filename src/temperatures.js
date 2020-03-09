5c00d6248428d9bb9a467ad20b67f3759fa2d23a
neoshh <48798309+neoshh@users.noreply.github.com>
2020-03-09 23:37:10 +0800

    added temperatures

---
+++ src/temperatures.js
@@ -0,0 +1,6 @@
+/** the initial temperatures for the thermostat
+    index 0 is the target temperature
+    index 1 is the current temperature **/
+const temperatures = [78, 60];
+
+export default temperatures;
